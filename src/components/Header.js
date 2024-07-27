9a17dfc1745f45aa6c97249b96bb947414d812fa
271915 (Abhishek Mehta) <abhishek.mehta@ics-global.in>
2024-07-28 01:50:59 +0530

    route and other setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <div>
+    <div className="z-10">
       <img
-        className="w-44 p-3 m-2 rounded-3xl bg-gradient-to-b from-black"
+        className="w-44 p-4 m-2 rounded-3xl bg-gradient-to-b from-black absolute z-20"
         src="https://w7.pngwing.com/pngs/198/254/png-transparent-replay-hd-logo.png"
         alt="logo"
       />
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,13 @@ const Login = ()=>{
     return(
         <div>
             <Header/>
+            <img className="absolute" src="https://images.unsplash.com/photo-1485095329183-d0797cdc5676?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+            <form className="absolute w-72 m-56 p-4 bg-slate-500">
+                <input type="text" placeholder="Email Address" className="p-2 m-2" />
+                <input type="password" placeholder="Password" className="p-2 m-2"/>
+                <button className="p-2 m-4 bg-slate-300 rounded-xl">Sign In</button>
+                <p>Are you new to netflix? Sign Up now!</p>
+            </form>
         </div>
     )
 }
